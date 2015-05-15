add1b98fef977f272e90eb0e34de9eca2d8c4fe3
cockroa4es <cockroa4es@gmail.com>
2015-05-15 10:18:22 +0300

    Initial commit

---
+++ simplify_agro.js
@@ -0,0 +1,26 @@
+
+function test() {
+    var result = simplify(points, 1, false);
+    var path = newPolygon.getPath();
+    var path2 = path.j;
+    var result2 = simplify(path2, 0.00001, true);
+    var str = GetCoordinate(result2);
+    var a = 5;
+}
+
+import {simplify} from "simplify.js";
+function simplifyPolygon(object, tolerance, highestQuality) {
+
+    var paths = object.getPath();
+    for (var i = 0; i < paths.length; i++) {
+        simplify(newPolygon.getPath().j, tolerance, highestQuality);
+    }
+
+    return object;
+}
+
+function simplifyStringPath(object, tolerance, highestQuality) {
+
+
+    return null;
+}
